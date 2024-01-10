62226879ed4e360a8c3fd4d66cb7dedb3ac75900
Shaikh Mohammed Talib <shaikhtalib2705@gmail.com>
2024-01-10 18:11:25 +0530

    commented Consoles

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-// console.log(2)
-
 let btn = document.querySelectorAll('button')
 let input = document.getElementById('input')
-
 
 // console.log(btn)
 // console.log(input)
@@ -16,38 +13,30 @@ btns.forEach(elem => {
     // console.log(elem)
     elem.addEventListener('click', (e) => {
         // console.log(e.target.innerHTML)
-
         if (e.target.innerHTML == 'AC') {
             arr = [];
             input.innerHTML = '';
         }
         else if (e.target.innerHTML == '=') {
-            console.log(eval(str))
+            // console.log(eval(str))
             input.innerHTML = eval(str);
         }
         else if (e.target.innerHTML == 'back') {
             arr.pop()
             str = arr
             input.innerHTML = str;
-            console.log(arr)
+            // console.log(arr)
         }
         // else if (e.target.innerHTML == '+') {
         //     e.target.innerHTML = `<button disabled>+</button>`
         // }
         else {
             arr.push(e.target.innerHTML)
-            console.log(arr)
+            // console.log(arr)
             str = arr.join('')
-            console.log(str)
+            // console.log(str)
             input.innerHTML = str;
         }
     })
 })
 
-
-
-let arr2 = [1, 2, 3, 4, 5]
-
-console.log(arr2)
-arr2.pop()
-console.log(arr2)
