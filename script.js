c83c31b7d7ea4989efb63b86b9e903b62e8ba2bd
Shaikh Mohammed Talib <shaikhtalib2705@gmail.com>
2024-01-13 21:52:11 +0530

    added Error Handling

--- script.js
+++ script.js
@@ -12,31 +12,40 @@ let arr = []
 btns.forEach(elem => {
     // console.log(elem)
     elem.addEventListener('click', (e) => {
+
         // console.log(e.target.innerHTML)
-        if (e.target.innerHTML == 'AC') {
-            arr = [];
-            input.innerHTML = '';
+        try {
+
+
+            if (e.target.innerHTML == 'AC') {
+                arr = [];
+                input.innerHTML = '';
+            }
+            else if (e.target.innerHTML == '=') {
+                // console.log(eval(str))
+                input.innerHTML = eval(str);
+            }
+            else if (e.target.innerHTML == 'back') {
+                arr.pop()
+                str = arr
+                input.innerHTML = str;
+                // console.log(arr)
+            }
+            // else if (e.target.innerHTML == '+') {
+            //     e.target.innerHTML = `<button disabled>+</button>`
+            // }
+            else {
+                arr.push(e.target.innerHTML)
+                // console.log(arr)
+                str = arr.join('')
+                // console.log(str)
+                input.innerHTML = str;
+            }
+        } catch (err) {
+            console.log(err.message);
+            input.innerHTML = 'Error';
         }
-        else if (e.target.innerHTML == '=') {
-            // console.log(eval(str))
-            input.innerHTML = eval(str);
-        }
-        else if (e.target.innerHTML == 'back') {
-            arr.pop()
-            str = arr
-            input.innerHTML = str;
-            // console.log(arr)
-        }
-        // else if (e.target.innerHTML == '+') {
-        //     e.target.innerHTML = `<button disabled>+</button>`
-        // }
-        else {
-            arr.push(e.target.innerHTML)
-            // console.log(arr)
-            str = arr.join('')
-            // console.log(str)
-            input.innerHTML = str;
-        }
+
     })
 })
 
